a55ddeac9a190eb12efc56fc47ce76eefc76f155
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-08-18 20:50:08 +0900

    Initial commit

---
+++ src/main/java/com/example/ghostvillageadmin/GhostVillageAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghostvillageadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostVillageAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostVillageAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ghostvillageadmin/GhostVillageAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghostvillageadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostVillageAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
