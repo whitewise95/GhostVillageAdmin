1aa714dea3c210d32e2ff80b3c49c5326eeb44d9
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-09-07 22:12:52 +0900

    장비 진행중

---
+++ src/main/java/com/example/ghostvillageadmin/app/equipment/Equipment.java
@@ -0,0 +1,16 @@
+package com.example.ghostvillageadmin.app.equipment;
+
+import com.example.ghostvillageadmin.base.BaseDomainWithId;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 장비
+ * */
+@Getter
+@Setter
+@Entity
+public class Equipment extends BaseDomainWithId {
+    
+}
